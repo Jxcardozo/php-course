78bb45fec1a4c204734b9cac50db665490897567
Jonny <jcardozo@go.pasadena.edu>
2020-05-15 10:24:49 -0500

    initial php setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<! DOCTYPE html >
+ < html > 
+  < head > 
+    < título > PHP Store </ title > 
+  </ head > 
+  < body > 
+    <? Php 
+    // Usando dos barras diagonales, creamos comentarios en php. 
+    // Dentro de la etiqueta php, podemos insertar valores en nuestro HTML. 
+    echo  "<h1> ¡Bienvenido! </h1>" ;
+    ?> 
+  </ body > 
+</ html >
