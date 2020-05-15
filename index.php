2c209567f7f4cb3527258347c6a31d5456b86b36
Jonny <jcardozo@go.pasadena.edu>
2020-05-15 11:50:31 -0500

    initial php setup

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
   </ head > 
   < body > 
     <? Php 
+    $name = "PHP Store";
+    $credit = 1000;
     // Usando dos barras diagonales, creamos comentarios en php. 
     // Dentro de la etiqueta php, podemos insertar valores en nuestro HTML. 
-    echo  "<h1> ¡Bienvenido! </h1>" ;
+   echo "<h1>Welcome to ".$name."!</h1>";
+   echo "<h2>You have $".$credit." in your wallet.</h2>";";
     ?> 
   </ body > 
 </ html >
