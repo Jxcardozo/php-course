f65f57b88ffacd32033dfcaf88b461536e2a5206
Jonny <jcardozo@go.pasadena.edu>
2020-05-15 16:23:42 -0500

    initial php setup

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
   </ head > 
   < body > 
     <? Php 
-    $name = "PHP Store";
-    $credit = 1000;
+    $ name = "PHP Store";
+    $ credit = 1000;
     // Usando dos barras diagonales, creamos comentarios en php. 
     // Dentro de la etiqueta php, podemos insertar valores en nuestro HTML. 
-   echo "<h1>Welcome to ".$name."!</h1>";
-   echo "<h2>You have $".$credit." in your wallet.</h2>";
+   echo "<h1>Welcome to ".$ name."!</h1>";
+   echo "<h2>You have $".$ credit." in your wallet.</h2>";
    
    
     $ productos ['Computadora'] = 750; 
@@ -18,9 +18,17 @@
     $ productos ['iPhone'] = 1000; 
     $ productos ['Tostadora'] = 75; 
       
-   echo "A car costs $".$products['Car']."</p>";
-   foreach ( $ productos  como  $ clave => $ valor ) {
-   echo "<p> El" . $ clave . "costos" . $ valor . "</p>" ;
+   echo "A car costs $".$ products['Car']."</p>";
+   foreach ( $ productos  as  $ key => $ value ) {
+   echo "<p> El" . $ key . "cuesta" . $ value . "</p>" ;
    }
+   echo "<h2>Items you can afford</h2>";
+   
+   foreach($ products as $ key => $ value){
+   if($ value <= $ credit ){
+   echo "<p>".$ key."</p>"; 
+   }
+   }
+   ?>
   </body> 
 </html >
