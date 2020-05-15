194a9c73e33d82ad698af626049348ef97314736
Jonny <jcardozo@go.pasadena.edu>
2020-05-15 16:49:16 -0500

    initial php setup

--- index.php
+++ index.php
@@ -1,34 +1,38 @@
 <! DOCTYPE html >
  < html > 
   < head > 
-    < título > PHP Store </ title > 
+    <title> PHP Store </ title > 
   </ head > 
   < body > 
     <? Php 
-    $ name = "PHP Store";
-    $ credit = 1000;
+    $name = "PHP Store";
+    $credit = 1000;
     // Usando dos barras diagonales, creamos comentarios en php. 
     // Dentro de la etiqueta php, podemos insertar valores en nuestro HTML. 
-   echo "<h1>Welcome to ".$ name."!</h1>";
-   echo "<h2>You have $".$ credit." in your wallet.</h2>";
+   echo "<h1>Welcome to ".$name."!</h1>";
+   echo "<h2>You have $".$credit." in your wallet.</h2>";
    
    
-    $ productos ['Computadora'] = 750; 
-    $ productos ['Auto'] = 15000; 
-    $ productos ['iPhone'] = 1000; 
-    $ productos ['Tostadora'] = 75; 
+    $productos ['Computadora'] = 750; 
+    $productos ['Auto'] = 15000; 
+    $productos ['iPhone'] = 1000; 
+    $productos ['Tostadora'] = 75; 
       
-   echo "A car costs $".$ products['Car']."</p>";
-   foreach ( $ productos  as  $ key => $ value ) {
-   echo "<p> El" . $ key . "cuesta" . $ value . "</p>" ;
+   echo "A car costs $".$products['Car']."</p>";
+   foreach ( $productos  as  $key => $value ) {
+   echo "<p> El" . $key . "cuesta" . $value . "</p>" ;
    }
    echo "<h2>Items you can afford</h2>";
    
-   foreach($ products as $ key => $ value){
-   if($ value <= $ credit ){
-   echo "<p>".$ key."</p>"; 
+   foreach($products as $key => $value){
+   if($value <= $credit ){
+   echo "<p>".$key."</p>"; 
    }
-   }
+   
+   $amount=800;
+   $taxRate=0.0825;
+   $addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
+   echo $addedTax;
    ?>
   </body> 
 </html >
