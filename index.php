669e793fd392330b19485bc3d01a803314513b61
Jonny <jcardozo@go.pasadena.edu>
2020-05-15 14:37:29 -0500

    initial php setup

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
     // Dentro de la etiqueta php, podemos insertar valores en nuestro HTML. 
    echo "<h1>Welcome to ".$name."!</h1>";
    echo "<h2>You have $".$credit." in your wallet.</h2>";";
+   productos ['Computadora'] = 750; 
+   $ productos ['Auto'] = 15000; 
+   $ productos ['iPhone'] = 1000; 
+   $ productos ['Tostadora'] = 75;
     ?> 
   </ body > 
 </ html >
